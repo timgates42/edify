01e0722e3e1df9db402941dd8fe3f738417d60ea
Tim Gates <tim.gates@iress.com>
2020-02-06 22:03:36 +1100

    Add the initial echo based procedure

---
+++ edify.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# The main entry point to start the edification
+
+echo install deadsnakes ppa
+echo install python3.8
+echo install pip
+echo install pipenv
+echo install Pipefile requirements
+echo run edify.py
