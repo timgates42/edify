3c9f3c6e3a6c7f66103b9fef47c57cecd1b7fd60
Tim Gates <tim.gates@iress.com>
2020-02-07 20:20:35 +1100

    install pathogen

--- edification/vimsetup.py
+++ edification/vimsetup.py
@@ -1,6 +1,7 @@
 
 import pathlib
 import shutil
+import subprocess
 
 from edification.storage import get_basedir
 
@@ -8,3 +9,17 @@ def vimsetup():
     src = get_basedir() / "data" / ".vimrc"
     trg = pathlib.Path.home() / ".vimrc"
     shutil.copy(src, trg)
+    pathogen()
+
+def pathogen():
+    autoload = pathlib.Path.home() / ".vim" / "autoload"
+    paths = [
+        pathlib.Path.home() / ".vim",
+        pathlib.Path.home() / ".vim" / "bundle",
+    ]
+    for path in paths:
+        path.mkdir(exist_ok=True, parents=True)
+    target = autoload / "pathogen.vim"
+    subprocess.check_call([
+        "curl", "-LSso", str(target), "https://tpo.pe/pathogen.vim"
+    ])
