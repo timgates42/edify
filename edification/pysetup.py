09a0c878ba22cd31cdc867565dd8f2dd50b0199c
Tim Gates <tim.gates@iress.com>
2020-02-10 20:12:33 +1100

    cleanup

--- edification/pysetup.py
+++ edification/pysetup.py
@@ -2,7 +2,6 @@
 cleanup python
 """
 
-import pathlib
 import subprocess
 
 
@@ -12,9 +11,11 @@ def pysetup():
     """
     cleanup_py38_mess()
 
+
 def cleanup_py38_mess():
-    # We needed these to get here but now they get in the way of the system
-    # python so drop them.
-    subprocess.call([
-        "sudo", "apt-get", "remove", "python3.8-distutils", "python3.8-lib2to3"
-    ])
+    """
+    We needed these to get here but now they get in the way of the system
+    python so drop them.
+    """
+    aptremove = ["sudo", "apt-get", "remove", "-y"]
+    subprocess.call(aptremove + ["python3.8-distutils", "python3.8-lib2to3"])
