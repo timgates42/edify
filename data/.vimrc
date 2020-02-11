b31c1d6ba6f22119d585d2e1eb30bfe2e338df8d
Tim Gates <tim.gates@iress.com>
2020-02-11 17:46:50 +1100

    Cleanup vim integration with standard python

--- data/.vimrc
+++ data/.vimrc
@@ -11,7 +11,7 @@ call vundle#begin()
 Plugin 'VundleVim/Vundle.vim'
 
 Plugin 'psf/black'
-Plugin 'timgates42/vim-isort'
+Plugin 'fisadev/vim-isort'
 Plugin 'vim-syntastic/syntastic'
 Plugin 'tpope/vim-fugitive'
 Plugin 'tpope/vim-dispatch'
--- edification/pysetup.py
+++ edification/pysetup.py
@@ -9,13 +9,40 @@ def pysetup():
     """
     runs python setup
     """
-    cleanup_py38_mess()
+    check = subprocess.call(
+        ["python3.6", "-c", "__import__('isort');__import__('black')"]
+    )
+    if check == 0:
+        return
+    exit_py38()
+    try:
+        setup_regular_py()
+    finally:
+        enter_py38()
 
 
-def cleanup_py38_mess():
+def enter_py38():
     """
-    We needed these to get here but now they get in the way of the system
-    python so drop them.
+    After installing standard python pip and package we resume py 3.8
+    """
+    aptins = ["sudo", "apt-get", "install", "-y"]
+    subprocess.call(aptins + ["python3.8-distutils", "python3.8-lib2to3"])
+
+
+def exit_py38():
+    """
+    To install standard python pip we need to temporarily remove these
     """
     aptremove = ["sudo", "apt-get", "remove", "-y"]
     subprocess.call(aptremove + ["python3.8-distutils", "python3.8-lib2to3"])
+
+
+def setup_regular_py():
+    """
+    Install python 3.6 isort
+    """
+    aptins = ["sudo", "apt-get", "install", "-y"]
+    subprocess.call(aptins + ["python3-distutils"])
+    subprocess.call(["sudo", "python3.6", "get-pip.py"])
+    subprocess.call(["sudo", "python3.6", "-m", "pip", "install", "isort"])
+    subprocess.call(["sudo", "python3.6", "-m", "pip", "install", "black"])
