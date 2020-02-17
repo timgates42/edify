9f641655aa0bf49eaf599283609f01d4ad079e6f
Tim Gates <tim.gates@iress.com>
2020-02-17 22:16:31 +1100

    fix url

--- data/bashrc.sh
+++ data/bashrc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export GHR=ssh://git@github.com/
+export GHR=ssh://git@github.com
 export GH=ssh://git@github.com/oneiress
 export GHTG=ssh://git@github.com/timgates42
 export EDITOR=vim
